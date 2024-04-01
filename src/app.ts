5f3bbb1f9cc2e8e6d53251f1111ae6bab1c670e5
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-01 20:47:19 -0300

    feat: create first route (create event)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
 import fastify from 'fastify'
+import { z } from 'zod'
 
 export const app = fastify()
+
+app.post('/events', (req, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive(),
+  })
+
+  const {
+    title,
+    details,
+    maximumAttendees
+  } = createEventSchema.parse(req.body)
+
+  return reply.status(201).send({
+    event: {
+      title,
+      details,
+      maximumAttendees
+    }
+  })
+})

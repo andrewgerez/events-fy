8a0b29c122c4f3bf8c36d0a6a5ad0df453785c86
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-02 21:45:36 -0300

    feat: register for event - route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
+import { registerForEvent } from './routes/register-for-event'
 
 export const app = fastify()
 
@@ -8,3 +9,4 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
+app.register(registerForEvent)

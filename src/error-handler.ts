fa1d52fd8190017ee90ab71adaefdac055bd2c90
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-03 23:33:55 -0300

    feat: validation error handler

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,12 +1,22 @@
 import { FastifyInstance } from 'fastify'
 import { BadRequest } from './routes/_errors/bad-request'
+import { ZodError } from 'zod'
 
 type FastifyErrorHandler = FastifyInstance['errorHandler']
 
 export const errorHandler: FastifyErrorHandler = (err, req, reply) => {
+  const { validation, validationContext } = err
+
+  if (err instanceof ZodError) {
+    return reply.status(400).send({
+      message: `Error during validation`,
+      errors: err.flatten().fieldErrors,
+    })
+  }
+
   if (err instanceof BadRequest) {
     return reply.status(400).send({ message: err.message })
   }
 
-  return reply.status(500).send({ message: 'Something went wrong' })
+  return reply.status(500).send({ message: 'Internal server error!' })
 }

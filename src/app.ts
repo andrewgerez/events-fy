34daf2a79ee390391a9a4389170a7c80b63a5bb3
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-03 23:21:30 -0300

    feat: fastify swagger setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import fastify from 'fastify'
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-event'
@@ -8,6 +12,23 @@ import { checkIn } from './routes/check-in'
 import { getEventAttendees } from './routes/get-events-attendees'
 
 export const app = fastify()
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'events-fy',
+      description: 'Specifications for events-fy API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)

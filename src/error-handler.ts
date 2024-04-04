49f56cb808bb15a3c8708a70f26139c3dce39f3a
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-03 23:30:44 -0300

    feat: use badrequest error handler

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance } from 'fastify'
+import { BadRequest } from './routes/_errors/bad-request'
 
 type FastifyErrorHandler = FastifyInstance['errorHandler']
 
 export const errorHandler: FastifyErrorHandler = (err, req, reply) => {
+  if (err instanceof BadRequest) {
+    return reply.status(400).send({ message: err.message })
+  }
+
   return reply.status(500).send({ message: 'Something went wrong' })
 }

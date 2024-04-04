eefa6b5c900f188b3d0142d77b782a0621702a90
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-03 23:24:25 -0300

    feat: fastify ui summary & tags

--- src/routes/get-events-attendees.ts
+++ src/routes/get-events-attendees.ts
@@ -8,6 +8,8 @@ export async function getEventAttendees(app: FastifyInstance) {
     .withTypeProvider<ZodTypeProvider>()
     .get('/events/:eventId/attendees', {
       schema: {
+        summary: 'Get event attendees',
+        tags: ['attendees'],
         params: z.object({
           eventId: z.string().uuid(),
         }),
